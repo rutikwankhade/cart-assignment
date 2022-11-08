6a1db8607435e999dbf0b01497addcfbe0dc3e08
rutik wankhade <rutikwankhade2@gmail.com>
2022-11-08 19:48:04 +0530

    show total with discounts

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -53,7 +53,7 @@ const ProductCard = ({
         }
     }
 
-     const removeFromCart = (product) => {
+    const removeFromCart = (product) => {
         const index = cartItems.indexOf(product);
         if (index > -1) {
 
@@ -70,12 +70,12 @@ const ProductCard = ({
     return (
         <div className={` ${customCSS} border border-gray-100 rounded-lg shadow-xl shadow-gray-50 m-4  `}>
             <img src={product.image} alt="product_image" className={`${imgHeight} rounded-t-lg w-full object-cover`} />
-            <div className="p-2">
+            <div className="p-4">
                 <span className="text-sm font-semibold text-gray-500">{product.brand}</span>
                 <h2 className="text-md truncate">{product.title}</h2>
 
                 <div className="flex items-center">
-                    <span className="text-lg font-semibold">{Math.trunc(product.price - (product.price * (product.discount_in_percent / 100)))}</span>
+                    <span className="text-xl font-semibold">{Math.trunc(product.price - (product.price * (product.discount_in_percent / 100)))}</span>
                     <span className=" mx-2 line-through text-gray-500 text-sm">{product.price}</span>
                     <span className="mx-2 text-sm text-green-500 font-semibold">{product.discount_in_percent}% off</span>
                 </div>
@@ -87,13 +87,7 @@ const ProductCard = ({
                     </button> : <div></div>
                 }
 
-                  {showRemoveFromCartButton ?
-                    <button
-                        onClick={() => removeFromCart(product)}
-                        className="border m-2 p-2 rounded-lg hover:bg-red-400 hover:text-white w-full mx-auto font-semibold text-center">
-                       REMOVE FROM CART
-                    </button> : <div></div>
-                }
+
 
 
                 {showSaveForLaterButton ?
@@ -105,6 +99,13 @@ const ProductCard = ({
                     <div></div>
                 }
 
+                {showRemoveFromCartButton ?
+                    <button
+                        onClick={() => removeFromCart(product)}
+                        className="border m-2 p-2 rounded-lg hover:bg-red-400 hover:text-white w-full mx-auto font-semibold text-center">
+                        REMOVE FROM CART
+                    </button> : <div></div>
+                }
                 {showRemoveFromSavedButton ?
                     <button
                         onClick={() => removeFromSaved(product)}
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -5,7 +5,10 @@ const Cart = () => {
 
     const { cartItems, savedItems, totalCartItems } = useCart()
 
-
+    const calculateTotal = () => {
+     return cartItems.reduce((totalWithDiscount, item) => totalWithDiscount + Math.trunc(item.price - (item.price * (item.discount_in_percent / 100))), 0)
+        
+    }
 
 
     return (
@@ -41,8 +44,16 @@ const Cart = () => {
 
 
 
-            <div className="w-1/4 border p-10">
+            <div className="md:w-1/4 border bordare-gray-50 h-max rounded-lg border-gray-100 shadow-xl shadow-gray-50 mt-4 p-10">
                 <h1 className="text-2xl">{totalCartItems} items added to your cart</h1>
+
+                <div className="my-4">
+                    <span className="text-lg">Cart Total with discounts</span>
+                </div>
+                                <span className="text-3xl font-semibold ">    {calculateTotal()}</span>
+
+                <span className="mx-2 line-through text-gray-500 text-lg">    {cartItems.reduce((total, item) => total + item.price, 0)}</span>
+
             </div>
 
         </div>
